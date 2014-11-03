d3518312d15c486388fa6c41c3233bd496036d10
ouzor <ouzor@iki.fi>
2014-11-03 19:44:39 +0200

    Added data sources and licenses, updated README

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ DryTrip
 ======
 
 Source code for [DryTrip - Cycling trip planner](https://ouzor.shinyapps.io/drytrip/) R Shiny application and related data processing.
+
+### Development plans
+
+Features to be added
+* GPX upload for routes
+* FMI weather forecast data retrieval. This is actually already implemented, but it does not work in the shinyapps.io service. I am considering to setup Shiny on the okf.fi server, which would solve this problem.
+* Weather forecast visualization improvements
+ * Show more data
+ * Show sunset time
+
+Want to report a bug or suggest a feature? Make a pull request or send me an email (ouzor at iki dot fi)!
+
+### License
+
+This work is lisenced under the [Creative Commons Attribution 4.0 International license](http://creativecommons.org/licenses/by/4.0/).
--- drytrip-shiny/ui.R
+++ drytrip-shiny/ui.R
@@ -10,67 +10,76 @@ shinyUI(fluidPage(
     sidebarPanel(
       # Adjust size
       tags$head(
-        tags$style(type="text/css", "select { width: 200px; }"),
-        tags$style(type="text/css", "textarea { max-width: 180px; }"),
-        tags$style(type="text/css", ".jslider { max-width: 200; }"),
-        tags$style(type='text/css', ".well { max-width: 200px; }"),
-        tags$style(type='text/css', ".span4 { max-width: 200px; }")
+        tags$style(type="text/css", "select { width: 280px; }"),
+        tags$style(type="text/css", "textarea { max-width: 260px; }"),
+        tags$style(type="text/css", ".jslider { max-width: 280; }"),
+        tags$style(type='text/css', ".well { max-width: 280px; }"),
+        tags$style(type='text/css', ".span4 { max-width: 280px; }")
       ),
-      helpText("Plan your cycling trips based on weather forecast!"),
-      p("Made by", a("@ouzor", href="https://twitter.com/ouzor")),
-      p("Source code in", a("GitHub", href="https://github.com/ouzor/drytrip", target="_blank")),
-      p("Data sources used: ADD")
+      h4("Plan your cycling trips based on weather forecast!"),
+      p("Data sources and R packages used:"),
+      tags$ul(
+        tags$li(a("Reittiopas API", href="http://developer.reittiopas.fi/pages/en/home.php")),
+        tags$li(a("Stamen maps", href="http://maps.stamen.com/#terrain/12/37.7706/-122.3782"),
+                "with",  a("ggmap", href="https://sites.google.com/site/davidkahle/ggmap")),
+        tags$li(a("Forecast API", href="https://developer.forecast.io/"),
+                "with", a("Rforecastio", href="https://github.com/ouzor/Rforecastio")),
+        tags$li(a("FMI Open Data", href="http://en.ilmatieteenlaitos.fi/open-data-manual-fmi-wfs-services"),
+                "with", a("fmi", href="https://github.com/ropengov/fmi"), "(not functional yet!)")
+      ),
+      p("Source code and development plans at", a("GitHub", href="https://github.com/ouzor/drytrip", target="_blank")),
+      p("License:", a("CC BY 4.0", href="http://creativecommons.org/licenses/by/4.0/")),
+      p("Made by", a("@ouzor", href="https://twitter.com/ouzor"))
     ),
     mainPanel(
       #       tabsetPanel(
       #         tabPanel("Route",
       
       h3("Define cycling route"),
-      selectInput("routeSource", "Route source", choices=c("Journey Planner API" = "journeyplanner", "GPX upload" = "gpxupload"), selected="journeyplanner"),
+      selectInput("routeSource", "Route source", choices=c("Reittiopas API" = "reittiopas", "GPX upload" = "gpxupload"), selected="reittiopas"),
       
       conditionalPanel(
-        condition = "input.routeSource == 'journeyplanner'",
+        condition = "input.routeSource == 'reittiopas'",
         textInput("start.address", "Start address", value = "Kamppi"),
         textInput("end.address", "End address", value = "Kamppi"),
-        textInput("via.addresses", "Via (separate with ';')", value= "Olari;Nuuksio;Leppävaara")
+        textInput("via.addresses", "Via (separate with ';')", value= "Olari;Nuuksio;Leppävaara"),
         #  submitButton("Submit"),
         # http://stackoverflow.com/questions/17704182/r-submitbutton-and-conditionalpanel
+        h3("Route info"),
+        sliderInput("cycling.speed", "Cycling speed (km/h)", min=10, max=40, value=20, step = 5),
+        # sliderInput("waypoint.interval", "Waypoint interval (minutes)", min=60, max=120, value=60, step=60),
+        htmlOutput("route_info"),
+        h4("Route map and waypoints"),
+        p("Note! Waypoints are currently set every one hour. This can be adjusted in the future."),
+        plotOutput("route_map_ggplot"),
+        #                    h3("Plot route profile"),
+        #                    p("Note! x-axis needs to be fixed to match the distance!"),
+        #                    plotOutput("route_profile_ggplot")
+        h3("Weather forecast"),
+        selectInput("forecastSource", "Weather forecast source",
+                    choices=c("Forecast.io" = "fio", "FMI" = "fmi"), selected="fio"),
+        conditionalPanel(
+          condition = "input.forecastSource == 'fio'",
+          sliderInput("forecast.length", "Number of starting hours to show",
+                      min=1, max=10, value=5, step = 1),
+          h4("Weather forecast at each waypoint for different starting hours"),
+          uiOutput("fio_plot_ui"), # plotOutput("fio_plot")
+          p("Known issues: Start times are in GMT+0 time zone (two hours less than in Finland). Latest start time is 24:00.")
+        ),
+        
+        conditionalPanel(
+          condition = "input.forecastSource == 'fmi'",
+          h3("FMI weather forecast feature to be implemented!")
+          #       htmlOutput("fmi_gvistable")
+        )
       ),
       
       conditionalPanel(
         condition = "input.routeSource == 'gpxupload'",
-        h3("Feature to be implemented")
-      ),
-      
-      
-      h3("Route info"),
-      sliderInput("cycling.speed", "Cycling speed (km/h)", min=10, max=40, value=20, step = 5),
-      # sliderInput("waypoint.interval", "Waypoint interval (minutes)", min=60, max=120, value=60, step=60),
-      htmlOutput("route_info"),
-      h4("Route map and waypoints"),
-      p("Note! Waypoints are currently set every one hour. This can be adjusted in the future."),
-      plotOutput("route_map_ggplot"),
-      #                    h3("Plot route profile"),
-      #                    p("Note! x-axis needs to be fixed to match the distance!"),
-      #                    plotOutput("route_profile_ggplot")
-      
-      h3("Weather forecast"),
-      selectInput("forecastSource", "Weather forecast source",
-                  choices=c("Forecast.io" = "fio", "FMI" = "fmi"), selected="fio"),
-      conditionalPanel(
-        condition = "input.forecastSource == 'fio'",
-        sliderInput("forecast.length", "Number of starting hours to show",
-                    min=1, max=10, value=5, step = 1),
-        h4("Weather forecast at each waypoint for different starting hours"),
-        uiOutput("fio_plot_ui"), # plotOutput("fio_plot")
-        p("Known issues: Start times are in GMT+0 time zone (two hours less than in Finland). Latest start time is 24:00.")
-      ),
-      
-      conditionalPanel(
-        condition = "input.forecastSource == 'fmi'",
-        h3("Feature to be implemented")
-        #       htmlOutput("fmi_gvistable")
+        h3("GPX upload feature to be implemented!")
       )
+
+
       
       #         tabPanel("Accidents",
       #                  h3("Plot bike accidents data and your route"),
